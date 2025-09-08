0cc16a8922328f582987e2af80186fc3284da6fa
DmitriyALEX <doiroffical@gmail.com>
2025-09-08 17:28:06 +0300

    fix: relocate HashRouter to App from index

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,25 @@ import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import PeoplePage from './pages/PeoplePage';
 import NotFoundPage from './pages/NotFoundPage';
+import { HashRouter as Router } from 'react-router-dom';
 
 export const App = () => (
   <div data-cy="app">
     <main className="section">
       <div className="container">
-        <Navbar />
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route path={'/'} element={<HomePage />} />
-          <Route path="/home" element={<Navigate to="/" replace />} />
-          <Route path={'people'}>
-            <Route index element={<PeoplePage />} />
-            <Route path={':slug'} element={<PeoplePage />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route index element={<HomePage />} />
+            <Route path={'/'} element={<HomePage />} />
+            <Route path="/home" element={<Navigate to="/" replace />} />
+            <Route path={'people'}>
+              <Route index element={<PeoplePage />} />
+              <Route path={':slug'} element={<PeoplePage />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Router>
       </div>
     </main>
   </div>
